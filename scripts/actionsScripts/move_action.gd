489bff551088310c3906789605ad390e78e65da1
KadenHolomego <kadendh2000@gmail.com>
2026-03-16 19:42:34 -0700

    movement fixed try2

--- scripts/actionsScripts/move_action.gd
+++ scripts/actionsScripts/move_action.gd
@@ -1,29 +1,31 @@
 extends Area2D
 
-@onready var ui = $".."
-@onready var player1 = $"../../players/Player1"
-@onready var player2 = $"../../players/Player2"
-@onready var player3 = $"../../players/Player3"
-@onready var player4 = $"../../players/Player4"
-var players: Array[Node2D] = []
+#@onready var player1 = $"../../players/Player1"
+#@onready var player2 = $"../../players/Player2"
+#@onready var player3 = $"../../players/Player3"
+#@onready var player4 = $"../../players/Player4"
+#var players: Array[Node2D] = []
 
 func _ready() -> void:
-	players = [player1, player2, player3, player4]
+	#players = [player1, player2, player3, player4]
+	pass
 
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+func move(viewport: Node, event: InputEvent, shape_idx: int, player, name) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		if GameManager.actionCount <= 0:
 			print("No actions remaining!")
 			return
-		var player = players[GameManager.currentPlayer]
-		
 		var current_city = Map.findCity(player.current_city)
+		#print(player.current_city)
+		print(name)
 		var is_connected = false
 		for i in current_city.get_num_of_connections():
 			if current_city.get_connection_name(i) == name:
 				is_connected = true
 		if is_connected:
-			player.global_position = global_position
+			var position = get_node("/root/Board/cityNodes/" + name)
+			player.global_position = position.global_position
+			print(position.global_position)
 			player.current_city = name
 			GameManager.actionCount -= 1
 		else:
--- scripts/mapScripts/cityClicked.gd
+++ scripts/mapScripts/cityClicked.gd
@@ -2,9 +2,19 @@ extends Area2D
 
 signal city_clicked(city_name)
 
-func _ready():
-	input_pickable = true # allows detection of the mouse
+@onready var player1 = $"../../players/Player1"
+@onready var player2 = $"../../players/Player2"
+@onready var player3 = $"../../players/Player3"
+@onready var player4 = $"../../players/Player4"
 
-func _input_event(viewport, event, shape_idx): #sets up input event for mouse buttons using default godot designations
+var players: Array[Node2D] = []
+
+func _ready():
+	input_pickable = true
+	players = [player1, player2, player3, player4]
+
+func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		emit_signal("city_clicked", name)
+		#print(players[GameManager.currentPlayer].current_city)
+		MoveAction.move(viewport, event, shape_idx, players[GameManager.currentPlayer], name)
