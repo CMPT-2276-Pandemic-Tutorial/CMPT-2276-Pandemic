bd73102965968e4a11f9df1c612049af003bb64e
PigletP <emily.r.taylor2002@gmail.com>
2026-03-17 23:36:22 -0700

    Updated movement action
    - Movement now requires double click to occur
    - Movement between two cities with a research station is now possible
    - Movement to a city you have the card for is now possible

--- scripts/actionsScripts/move_action.gd
+++ scripts/actionsScripts/move_action.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var last_city_pressed
+
 #@onready var player1 = $"../../players/Player1"
 #@onready var player2 = $"../../players/Player2"
 #@onready var player3 = $"../../players/Player3"
@@ -10,28 +12,64 @@ func _ready() -> void:
 	#players = [player1, player2, player3, player4]
 	pass
 
-func move(viewport: Node, event: InputEvent, shape_idx: int, player, name) -> void:
+func move(viewport: Node, event: InputEvent, shape_idx: int, player, destination) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if last_city_pressed != destination: 
+			last_city_pressed = destination
+			return
 		if GameManager.actionCount <= 0:
 			print("No actions remaining!")
 			return
 		var current_city = Map.findCity(player.current_city)
 		#print(player.current_city)
-		print(name)
-		var is_connected = false
+		print(destination)
+		var is_des_connected = false
 		for i in current_city.get_num_of_connections():
-			if current_city.get_connection_name(i) == name:
-				is_connected = true
-		if is_connected:
-			var position = get_node("/root/Board/cityNodes/" + name)
-			player.global_position = position.global_position
-			print(position.global_position)
-			player.current_city = name
+			if current_city.get_connection_name(i) == destination:
+				is_des_connected = true
+		if is_des_connected:
+			var board_position = get_node("/root/Board/cityNodes/" + destination)
+			player.global_position = board_position.global_position
+			print(board_position.global_position)
+			player.current_city = destination
 			if GameManager.playerRole[GameManager.currentPlayer] == "Quarantine Specialist":
 				Map.resetProtection()
 				current_city.set_protection(true)
 				for i in current_city.get_num_of_connections():
 					Map.findCity(current_city.get_connection_name(i)).set_protection(true)
 			GameManager.actionCount -= 1
-		else:
-			print("invalid move")
+			return
+		
+		#check if shuttle flight is possible
+		if current_city.get_station() and Map.findCity(destination).get_station():
+			var board_position = get_node("/root/Board/cityNodes/" + destination)
+			player.global_position = board_position.global_position
+			print(board_position.global_position)
+			player.current_city = destination
+			if GameManager.playerRole[GameManager.currentPlayer] == "Quarantine Specialist":
+				Map.resetProtection()
+				current_city.set_protection(true)
+				for i in current_city.get_num_of_connections():
+					Map.findCity(current_city.get_connection_name(i)).set_protection(true)
+			GameManager.actionCount -= 1
+			return
+		
+		#check if direct flight is possible
+		var player_hand = PlayerHand.player_hand[GameManager.currentPlayer]
+		for card in player_hand.size():
+			var data = player_hand[card].card_data
+			if typeof(data) == TYPE_DICTIONARY and data.get("name") == destination:
+				var board_position = get_node("/root/Board/cityNodes/" + destination)
+				player.global_position = board_position.global_position
+				print(board_position.global_position)
+				player.current_city = destination
+				if GameManager.playerRole[GameManager.currentPlayer] == "Quarantine Specialist":
+					Map.resetProtection()
+					current_city.set_protection(true)
+					for i in current_city.get_num_of_connections():
+						Map.findCity(current_city.get_connection_name(i)).set_protection(true)
+				PlayerHand.remove_card_from_hand(player_hand[card])
+				GameManager.actionCount -= 1
+				return
+		
+		print("invalid move")
