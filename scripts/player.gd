f0bc1621a48ca6d3c0600e0f2b360d47651f08a7
PigletP <emily.r.taylor2002@gmail.com>
2026-03-18 22:21:34 -0700

    added offset to players at start

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+@export var x_offset:float
+@export var y_offset:float
+
 var current_city: String = "Atlanta"
 
+
 func _ready() -> void:
-	global_position = $"../../cityNodes/Atlanta".global_position
+	global_position = $"../../cityNodes/Atlanta".global_position + Vector2(x_offset,y_offset)
