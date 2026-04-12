5f21a48770d8d88ca9e45b0dc2ba5bde02d7c079
PigletP <emily.r.taylor2002@gmail.com>
2026-04-12 00:09:25 -0700

    Set up for automated testing
    - Added test screen, can be brought up by pressing the slash key while on the menu screen.
    - The game now exits when you press esc
    - Moved the button script to the main node in menu scene

--- scripts/UIScripts/startGameButton.gd
+++ scripts/UIScripts/startGameButton.gd
@@ -1,8 +1,14 @@
-extends Button
-var menu = "res://scenes/boardMain.tscn"
-var tutorial = "res://scenes/UIScenes/tutorialScenes/tutorial_menu.tscn"
+extends Node
+const menu = "res://scenes/boardMain.tscn"
+const tutorial = "res://scenes/UIScenes/tutorialScenes/tutorial_menu.tscn"
+const testing = "res://scenes/AutomatedTestScreen.tscn"
 
-func _on_pressed() -> void:
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_SLASH:
+			get_tree().change_scene_to_file(testing)
+
+func _on_start_select_pressed() -> void:
 	get_tree().change_scene_to_file(menu)
 
 
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -113,3 +113,9 @@ func outbreak(cityOutbreaking) -> void:
 			var chain = Map.findCity(cityOutbreaking.get_connection_name(i)).infect(cityOutbreaking.get_colour())
 			if chain: 
 				outbreak(Map.findCity(cityOutbreaking.get_connection_name(i)))
+
+#Exit game on esc key press
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			get_tree().quit()
